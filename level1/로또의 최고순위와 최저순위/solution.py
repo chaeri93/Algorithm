04db80d8a386e00273921d23c85714921c765483
chaeri93 <chaeri93@ewhain.net>
2022-06-12 13:18:08 +0900

    신규 아이디 추천

--- level1/로또의 최고순위와 최저순위/solution.py
+++ level1/로또의 최고순위와 최저순위/solution.py
@@ -1,13 +1,16 @@
 def solution(lottos, win_nums):
     answer = []
+    # 최저순위
     cnt = 7
+    # 최고순위
     best = 7
     for i in lottos:
         if i in win_nums:
             cnt -= 1
             best -= 1
-        elif i==0:
-            best -=1
+        elif i == 0:
+            best -= 1
+    # 7은 순위에 없기 때문에 6으로 치환
     if best > 6:
         answer.append(6)
     else:
@@ -19,7 +22,7 @@ def solution(lottos, win_nums):
 
     return answer
 
-lottos=[0, 0, 0, 0, 0, 0]
-win_nums=[38, 19, 20, 40, 15, 25]
-solution(lottos, win_nums)
 
+lottos = [0, 0, 0, 0, 0, 0]
+win_nums = [38, 19, 20, 40, 15, 25]
+solution(lottos, win_nums)
