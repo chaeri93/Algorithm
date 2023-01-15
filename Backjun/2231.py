764c936316408e1a6901ab4ad2ff105e559e294e
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-01-16 06:47:02 +0900

    [solved] 2231

---
+++ Backjun/2231.py
@@ -0,0 +1,10 @@
+n = int(input())
+result = 0
+for i in range(1,n+1):
+    arr = map(int, str(i))
+    result = i + sum(arr)
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
