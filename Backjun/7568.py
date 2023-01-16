0fa53bb995612a558289bc68039af844b549dead
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-01-16 22:47:42 +0900

    [solved] 7568

---
+++ Backjun/7568.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = []
+for i in range(n):
+    w, h = map(int, input().split())
+    arr.append((w, h))
+
+for i in arr:
+    r = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+            r += 1
+    print(r, end=" ")
