48dc9c9e4740a4b5a7dffea9448b54cea3d22bb9
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-01-18 23:57:48 +0900

    [solved] 22479

---
+++ Backjun/24479.py
@@ -0,0 +1,31 @@
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+cnt = 1
+
+
+def dfs(graph, visited, v):
+    global cnt
+    visited[v] = cnt
+
+    for i in graph[v]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(graph, visited, i)
+
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(n + 1):
+    graph[i].sort()
+
+dfs(graph, visited, r)
+
+for i in range(n + 1):
+    if i != 0:
+        print(visited[i])
