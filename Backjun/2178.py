84cc0a0f134d7935a92bc0994e073a3dff3c8513
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-01-24 01:51:48 +0900

    [solved] 2178

---
+++ Backjun/2178.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10 ** 6)
+
+m, n = map(int, sys.stdin.readline().split())
+graph = []
+for _ in range(m):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    queue = deque([(x,y)])
+
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+
+    return graph[m-1][n-1]
+
+
+print(bfs(0, 0))
