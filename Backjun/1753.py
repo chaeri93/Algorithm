458db843774ac5111f110b234966db3dfee6275b
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-06-25 12:37:10 +0900

    요격시스템

--- Backjun/1753.py
+++ Backjun/1753.py
@@ -1,1 +1,11 @@
 import sys
+
+v, e = map(int, sys.stdin.readline().split())
+start = int(sys.stdin.readline())-1
+dist = [float('inf')]*v
+dist[start] = 0
+
+linked = [[] for _ in range(v)]
+for _ in range(e):
+    a,b,w = map(int, sys.stdin.readline().split())
+    
