1db7b239fe0380e6b9f397468d2683e7bdb8f77d
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-09-06 23:11:00 +0900

    [solved] 1388

---
+++ Backjun/1388.py
@@ -0,0 +1,30 @@
+def solution(x, y):
+    if struct[x][y] == "-":
+        struct[x][y] = 1
+        for _y in [1, -1]:
+            Y = y + _y
+            if (Y > 0 and Y < m) and struct[x][Y] == '-':
+                solution(x, Y)
+
+    if struct[x][y] == "|":
+        struct[x][y] = 1
+        for _x in [1, -1]:
+            X = x + _x
+            if (X > 0 and X < n) and struct[X][y] == '|':
+                solution(X, y)
+
+
+n, m = map(int, input().split())
+struct = []
+for _ in range(n):
+    struct.append(list(input()))
+
+count = 0
+
+for i in range(n):
+    for j in range(m):
+        if struct[i][j] == "-" or struct[i][j] == "|":
+            solution(i, j)
+            count += 1
+
+print(count)
