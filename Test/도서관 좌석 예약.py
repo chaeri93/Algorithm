1bcde6ba0a9496bb549db91e9f92baba28069df3
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-09-08 16:38:08 +0900

    [solved] 도서관 좌석 예약

---
+++ Test/도서관 좌석 예약.py
@@ -0,0 +1,23 @@
+"""
+EPPER 도서관 좌석 예약
+https://level.goorm.io/exam/100917/%EB%8F%84%EC%84%9C%EA%B4%80-%EC%A2%8C%EC%84%9D-%EC%98%88%EC%95%BD/quiz/1
+"""
+
+
+def solution(s, e, N):
+    answer = 0
+    tmp = [[i, j] for i, j in zip(s, e)]
+    tmp.sort(key=lambda x: (x[1], x[1]-x[0]))
+    e1 = e2 = -1
+
+    for t in tmp:
+        if e1 <= t[0]:
+            answer += 1
+            e1 = t[1]
+        elif e2 <= t[0]:
+            answer += 1
+            e2 = e1
+            e1 = t[1]
+
+    return answer
+
