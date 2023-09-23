9ce34c0d5011cc9d0f36b137577ea6fc40cc762d
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-09-23 14:09:37 +0900

    [fix] 1388

--- Backjun/Silver/Lv4/1388.py
+++ Backjun/Silver/Lv4/1388.py
@@ -1,17 +1,29 @@
-def solution(x, y):
+def dfs(x, y):
     if struct[x][y] == "-":
         struct[x][y] = 1
         for _y in [1, -1]:
             Y = y + _y
             if (Y > 0 and Y < m) and struct[x][Y] == '-':
-                solution(x, Y)
+                dfs(x, Y)
 
     if struct[x][y] == "|":
         struct[x][y] = 1
         for _x in [1, -1]:
             X = x + _x
             if (X > 0 and X < n) and struct[X][y] == '|':
-                solution(X, y)
+                dfs(X, y)
+
+
+def solution(n, m, struct):
+    count = 0
+
+    for i in range(n):
+        for j in range(m):
+            if struct[i][j] == "-" or struct[i][j] == "|":
+                dfs(i, j)
+                count += 1
+
+    return count
 
 
 n, m = map(int, input().split())
@@ -19,12 +31,4 @@ struct = []
 for _ in range(n):
     struct.append(list(input()))
 
-count = 0
-
-for i in range(n):
-    for j in range(m):
-        if struct[i][j] == "-" or struct[i][j] == "|":
-            solution(i, j)
-            count += 1
-
-print(count)
+print(solution(n, m, struct))
