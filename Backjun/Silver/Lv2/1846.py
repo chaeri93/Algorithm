4945d5a274a20ce3f3282bf8257c9b73353ee848
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-12-24 23:52:48 +0900

    [solved] 1876 스택 수열

--- Backjun/Silver/Lv2/1846.py
+++ Backjun/Silver/Lv2/1846.py
@@ -1,29 +1,27 @@
-count = 1
-temp = True
-stack = []
-op = []
+if __name__ == '__main__':
+    count = 1
+    temp = True
+    stack = []
+    op = []
 
-N = int(input())
-for i in range(N):
-    num = int(input())
-    # num이하 숫자까지 스택에 넣기
-    while count <= num:
-        stack.append(count)
-        op.append('+')
-        count += 1
+    N = int(input())
+    for i in range(N):
+        num = int(input())
 
-    # num이랑 스택 맨 위 숫자가 동일하다면 제거
-    if stack[-1] == num:
-        stack.pop()
-        op.append('-')
-    # 스택 수열을 만들 수 없으므로 NO
+        while count <= num:
+            stack.append(count)
+            op.append('+')
+            count += 1
+
+        if stack[-1] == num:
+            stack.pop()
+            op.append('-')
+        else:
+            temp = False
+            break
+
+    if temp == False:
+        print("NO")
     else:
-        temp = False
-        break
-
-# 스택 수열을 만들수 있는지 여부에 따라 출력
-if temp == False:
-    print("NO")
-else:
-    for i in op:
-        print(i)
+        for i in op:
+            print(i)
