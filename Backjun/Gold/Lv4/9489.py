486207754f955ec3af58bf594e6989070ca9473c
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-01-07 23:41:01 +0900

    [solved] 4256 트리

--- Backjun/Gold/Lv4/9489.py
+++ Backjun/Gold/Lv4/9489.py
@@ -1,24 +1,25 @@
 import sys
-input=sys.stdin.readline
+
+input = sys.stdin.readline
 from collections import defaultdict
 
 while True:
 
-    N,K=map(int,input().split())
-    if [N,K]==[0,0]:
+    N, K = map(int, input().split())
+    if [N, K] == [0, 0]:
         break
-    L=list(map(int,input().split()))
-    Parent=defaultdict(int)
+    L = list(map(int, input().split()))
+    Parent = defaultdict(int)
 
-    index=0
-    for i in range(1,N):
-        Parent[L[i]]=L[index]
-        if i<N-1 and L[i]+1<L[i+1]:
-            index+=1
+    index = 0
+    for i in range(1, N):
+        Parent[L[i]] = L[index]
+        if i < N - 1 and L[i] + 1 < L[i + 1]:
+            index += 1
 
-    count=0
-    if Parent[Parent[K]]: # 부모의 부모가 존재한다면
+    count = 0
+    if Parent[Parent[K]]:
         for Node in L:
-            if (Parent[Parent[K]] == Parent[Parent[Node]]) and Parent[Node]!=Parent[K]: #부모의 부모가 같다면
-                count+=1
+            if (Parent[Parent[K]] == Parent[Parent[Node]]) and Parent[Node] != Parent[K]:
+                count += 1
     print(count)
