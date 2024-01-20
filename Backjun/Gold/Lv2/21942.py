c7fa70c12cab838e2f7afdbb1048cac2e5c19fc2
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-01-20 23:14:09 +0900

    [solved] 21942 부품 대여장

---
+++ Backjun/Gold/Lv2/21942.py
@@ -0,0 +1,32 @@
+import sys
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+input = sys.stdin.readline
+
+
+n, l, f = map(str, input().split())
+n, f, maximum = int(n), int(f), timedelta(days=int(l[:3]), hours=int(l[4:6]), minutes=int(l[7:]))
+minute = timedelta(minutes=1)
+table = defaultdict(dict)
+p_table = defaultdict()
+for _ in range(n):
+    line = input()
+    now = datetime.strptime(line[:16], '%Y-%m-%d %H:%M')
+    part, name = line[16:].split()
+    if not p_table.get(name):
+        p_table[name] = 0
+    if table[name].get(part):
+        period = now - table[name][part]
+        if period > maximum:
+            p_table[name] += ((period - maximum) // minute) * f
+        del table[name][part]
+    else:
+        table[name][part] = now
+
+ret = [(k, v) for k, v in p_table.items() if v]
+if ret:
+    for name, val in sorted(ret, key=lambda x: x[0]):
+        print(name, val)
+else:
+    print(-1)
