88fecf94d6dc2e81b6a23340b362e47ba5e6a142
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-01-27 23:09:02 +0900

    [solved] 9251 LCS

--- Backjun/Gold/Lv5/12865.py
+++ Backjun/Gold/Lv5/12865.py
@@ -1,21 +1,22 @@
 def solution(N, K, obj):
     knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 
-    for i in range(1, N+1):
-        for j in range(1, K+1):
+    for i in range(1, N + 1):
+        for j in range(1, K + 1):
             weight = obj[i][0]
             value = obj[i][1]
 
             if j < weight:
-                knapsack[i][j] = knapsack[i-1][j]
+                knapsack[i][j] = knapsack[i - 1][j]
             else:
-                knapsack[i][j] = max(value + knapsack[i-1][j-weight],knapsack[i-1][j])
+                knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
 
     return knapsack[N][K]
 
+
 if __name__ == "__main__":
     N, K = map(int, input().split())
-    obj = [[0,0]]
+    obj = [[0, 0]]
     for _ in range(N):
         obj.append(list(map(int, input().split())))
 
