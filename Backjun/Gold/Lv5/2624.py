4faf72043193b8067d57c3c2ba96520dd37064e2
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-01-28 23:40:47 +0900

    [solved] 2624 동전 바꿔주기

---
+++ Backjun/Gold/Lv5/2624.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+k = int(input())
+coin = [tuple(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (t + 1)
+dp[0] = 1
+for c, tmp in coin:
+    for money in range(t, 0, -1):
+        for i in range(1, tmp + 1):  # 현재 동전 coin의 개수만큼 for문 진행
+            if money - c * i >= 0:  # 0원 이상인 경우
+                dp[money] += dp[money - c * i]
+print(dp[t])
