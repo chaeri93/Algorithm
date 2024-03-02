098f32ab2368d27eb7a90979bfec8b575d45675d
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-03-02 23:58:04 +0900

    [solved] 16236 아기 상어

--- Backjun/Gold/Lv4/17140.py
+++ Backjun/Gold/Lv4/17140.py
@@ -5,8 +5,8 @@ A = [list(map(int, sys.stdin.readline().split())) for _ in range(3)]
 
 
 def sort_new(matrix, RC):
-    cnt = 0
     sorted_matrix = []
+    max_count = 0
     for i in range(len(matrix)):
         B = []
         dic = dict()
@@ -16,15 +16,14 @@ def sort_new(matrix, RC):
                     dic[matrix[i][j]] = 1
                 else:
                     dic[matrix[i][j]] += 1
-        for key, value in dic:
-            B.append((key, value))
-
+        for key, value in dic.items():
+            B.append([key, value])
         B.sort(key=lambda x: [x[1], x[0]])
         C = sum(B, [])
-        cnt = max(cnt, len(C))
+        max_count = max(max_count, len(C))
         sorted_matrix.append(C)
     for i in sorted_matrix:
-        i += [0] * (cnt - len(i))  # 가장 긴 길이에 맞춰서 0 추가
+        i += [0] * (max_count - len(i))  # 가장 긴 길이에 맞춰서 0 추가
         if len(i) > 100:
             i = i[:100]
     return sorted_matrix if RC == "R" else list(zip(*sorted_matrix))
