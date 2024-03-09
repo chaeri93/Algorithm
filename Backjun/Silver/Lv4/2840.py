c1dcc97fee1022aa08f6b9a622ccd539b253ab2d
chaeri93 <chaeri93@ewhain.net>
2024-03-09 23:07:27 +0900

    [solved] 2840 행운의 바퀴

---
+++ Backjun/Silver/Lv4/2840.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+record = [input().split() for _ in range(k)]
+
+def solution(n, record):
+    wheel = ['?'] * n
+    alphabet = dict()
+
+    a = ord('A')
+    for i in range(26):
+        alphabet[chr(a + i)] = True
+
+    idx = 0
+    for s, alpha in record:
+        idx = (idx - int(s)) % n
+        print(wheel, idx)
+        if wheel[idx] == alpha:
+            continue
+        if wheel[idx] != '?' or not alphabet[alpha]:
+            return '!'
+        wheel[idx] = alpha
+        alphabet[alpha] = False
+
+    return ''.join(wheel[idx:] + wheel[:idx])
+
+
+solution(n, record)
