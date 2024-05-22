f047bc2332cef256489d0e3efe14be81d47875f8
chaeri93 <chaeri93@ewhain.net>
2024-05-22 16:56:57 +0900

    [solved] 붕대감기

---
+++ Programmers/level2/붕대감기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> bandage, int health, vector<vector<int> > attacks) {
+    int answer = 0;
+    // while (health > 0 && attacks.size > 0) {
+        
+    // }
+    return answer;
+}
